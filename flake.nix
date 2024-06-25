3a9767fbbc69cf39d682d27aea12ac2f4d744cd4
Steve Dodd <steved424@gmail.com>
2024-06-25 20:44:33 +0100

    Create checks attr for buildbot

--- flake.nix
+++ flake.nix
@@ -5,10 +5,11 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }: rec {
 
     packages.x86_64-linux.default = (import ./simplest2.nix
             { pkgs = nixpkgs.legacyPackages.x86_64-linux; }).simplest;
 
+    checks.x86_64-linux.packages-default = packages.x86_64-linux.default;
   };
 }
