5762c40e82ecd4bc51e6c0012db80645cdb7db86
srd424 <srd424@users.noreply.github.com>
2024-07-04 22:10:07 +0100

    Update zerotier.nix

--- non-free/zerotier.nix
+++ non-free/zerotier.nix
@@ -2,7 +2,7 @@
 let
   # <nixpkgs> is set to the value designated by the nixpkgs input of the
   # jobset configuration.
-  pkgs = (import <nixpkgs> {});
+  pkgs = (import <nixpkgs> { config.allowUnfree = true; });
 in {
   zerotierone = pkgs.zerotierone;
 }
